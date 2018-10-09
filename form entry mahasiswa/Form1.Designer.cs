b3d58809ff3f20c1c4c6856a239c34ba57012a4c
Mahyosha <Mahyosha.muhammad@students.amikom.ac.id>
2018-10-09 19:11:48 +0700

    Tugas3PemrogramanLanjut
    Form Mahasiswa

---
+++ form entry mahasiswa/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace form_entry_mahasiswa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.txtNim = new System.Windows.Forms.MaskedTextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.rdoLakilaki = new System.Windows.Forms.RadioButton();
+            this.rdoPerempuan = new System.Windows.Forms.RadioButton();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.txtBorn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.Location = new System.Drawing.Point(13, 139);
+            this.lvwMahasiswa.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(556, 255);
+            this.lvwMahasiswa.TabIndex = 0;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            this.lvwMahasiswa.DoubleClick += new System.EventHandler(this.lvwMahasiswa_DoubleClick);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(586, 218);
+            this.btnTambah.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(100, 28);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "Simpan";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Location = new System.Drawing.Point(586, 158);
+            this.btnUbah.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(100, 28);
+            this.btnUbah.TabIndex = 1;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            this.btnUbah.Click += new System.EventHandler(this.btnUbah_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(586, 275);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(100, 28);
+            this.btnHapus.TabIndex = 1;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Location = new System.Drawing.Point(586, 330);
+            this.btnTutup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(100, 28);
+            this.btnTutup.TabIndex = 1;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            this.btnTutup.Click += new System.EventHandler(this.btnTutup_Click);
+            // 
+            // txtNim
+            // 
+            this.txtNim.Location = new System.Drawing.Point(119, 15);
+            this.txtNim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNim.Mask = "##.##.####";
+            this.txtNim.Name = "txtNim";
+            this.txtNim.Size = new System.Drawing.Size(92, 22);
+            this.txtNim.TabIndex = 2;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(570, 15);
+            this.txtAlamat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(307, 22);
+            this.txtAlamat.TabIndex = 3;
+            // 
+            // rdoLakilaki
+            // 
+            this.rdoLakilaki.AutoSize = true;
+            this.rdoLakilaki.Checked = true;
+            this.rdoLakilaki.Location = new System.Drawing.Point(119, 79);
+            this.rdoLakilaki.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoLakilaki.Name = "rdoLakilaki";
+            this.rdoLakilaki.Size = new System.Drawing.Size(89, 21);
+            this.rdoLakilaki.TabIndex = 4;
+            this.rdoLakilaki.TabStop = true;
+            this.rdoLakilaki.Text = "Laki - laki";
+            this.rdoLakilaki.UseVisualStyleBackColor = true;
+            this.rdoLakilaki.CheckedChanged += new System.EventHandler(this.rdoLakilaki_CheckedChanged);
+            // 
+            // rdoPerempuan
+            // 
+            this.rdoPerempuan.AutoSize = true;
+            this.rdoPerempuan.Location = new System.Drawing.Point(243, 79);
+            this.rdoPerempuan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoPerempuan.Name = "rdoPerempuan";
+            this.rdoPerempuan.Size = new System.Drawing.Size(102, 21);
+            this.rdoPerempuan.TabIndex = 4;
+            this.rdoPerempuan.Text = "Perempuan";
+            this.rdoPerempuan.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(119, 47);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(307, 22);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(570, 81);
+            this.dtpDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(183, 22);
+            this.dtpDate.TabIndex = 5;
+            this.dtpDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // txtBorn
+            // 
+            this.txtBorn.Location = new System.Drawing.Point(570, 47);
+            this.txtBorn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBorn.Name = "txtBorn";
+            this.txtBorn.Size = new System.Drawing.Size(307, 22);
+            this.txtBorn.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NAMA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Jenis Kelamin";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(459, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Alamat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(459, 50);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tempat Lahir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(459, 81);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Tanggal Lahir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 421);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.rdoPerempuan);
+            this.Controls.Add(this.rdoLakilaki);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtBorn);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtNim);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.lvwMahasiswa);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data mahasiswa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwMahasiswa;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTutup;
+        private System.Windows.Forms.MaskedTextBox txtNim;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.RadioButton rdoLakilaki;
+        private System.Windows.Forms.RadioButton rdoPerempuan;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtBorn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
